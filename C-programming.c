7a776215fd6ba4c0cd49eb704b3e1c886511a188
Pranav-Rasal <zcoer_ra1340_2025_26@zealeducation.com>
2026-09-16 19:11:58 +0530

    Turbo C++

---
+++ C-programming.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+    clrscr();
+    
+    char name[20];
+
+    printf("Enter your name:- ");
+
+    scanf("%s", name);
+
+    printf("Hello, Robotics!\n");
+
+     printf("Welcome, %s\n", name);
+
+    getchar();
+}   
+
